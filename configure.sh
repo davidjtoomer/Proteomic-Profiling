80d151ff1aabb108ea02b5081da879ff906b66eb
David Toomer <djtoomer22@gmail.com>
2020-04-07 02:21:54 -0400

    Add files via upload

--- configure.sh
+++ configure.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+# activate the virtual environment
+conda env create -f environment.yml
+source activate secreted-labeler
 
+# securely install pip and all dependencies
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install -r requirements.txt
+
+# make sure all dependencies are imported properly
 python testenvironment.py || exit 1
